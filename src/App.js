8e6241dc19e47639075430858a26cee7c7ea3866
Webster Grimes <cwebsterg@mac.com>
2022-11-07 12:09:12 -0500

    ugly data BUT displaying

--- src/App.js
+++ src/App.js
@@ -7,21 +7,35 @@ const App = ({bookData}) => {
       {bookData.map((book) => (
         <div className="book-card">
           <div>
-            <h2>{book.title}</h2>
+            <h2><a href={book.url}>{book.title}</a></h2>
             <h3>{book.author}</h3>
             <div className="short-description">{book.shortDescription}</div>
             <Description
-            url={book.url}
             publisher={book.publisher}
             date={book.publicationDate}
-            details={book.detailedDescription}/>
-            <img className="cover" src={book.coverImageUrl}/>
+            detail={book.detailedDescription}/>
           </div>
+            <img className="cover" src={book.coverImageUrl} alt="book cover"/>
         </div>
       ))}
     </div>
   )}
 
-const Description = 
+const Description = ({url, publisher, date, details}) => {
+  const [display, setDisplay] = useState(false)
+  const handleClick = (event) => {setDisplay(!display)}
+  return (
+    <>
+    <button onClick={handleClick} type="button">
+      {display ? "Hide" : "Show"} Details</button>
+    {display &&
+    <div className="details">
+      <div className="full-description">{details}</div>
+      <div className="publisher">Publisher: {publisher ? <>{publisher}</> : "Not Available"}</div>
+      <div className="date">Publication Date: {date ? <>{date}</> : "Not Available"}</div>
+    </div>}
+    </>
+  )};
+
 export default App;
 
