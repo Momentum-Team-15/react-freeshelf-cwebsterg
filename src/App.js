a1f5f610a45e5cf945ac841ccd16548245c13207
Webster Grimes <cwebsterg@mac.com>
2022-11-07 11:47:36 -0500

    god help me first (fifth redux) commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
-function App() {
+const App = ({bookData}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      {bookData.map((book) => (
+        <div className="book-card">
+          <div>
+            <h2>{book.title}</h2>
+            <h3>{book.author}</h3>
+            <div className="short-description">{book.shortDescription}</div>
+            <Description
+            url={book.url}
+            publisher={book.publisher}
+            date={book.publicationDate}
+            details={book.detailedDescription}/>
+            <img className="cover" src={book.coverImageUrl}/>
+          </div>
+        </div>
+      ))}
     </div>
-  );
-}
+  )}
 
+const Description = 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import data from "./books.json"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App bookData={data.books} />
   </React.StrictMode>
 );
 
